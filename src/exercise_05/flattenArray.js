2bf9de011c30d184b737217bd87fed7900e36ad2
Lingzhen Kong <1072476988@qq.com>
2020-09-09 17:33:53 +0800

    [lingzhen] task: update flattenArray.js.

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,21 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === null || array === undefined) {
+    throw new Error('Flatten undefined or null array');
+  }
+
+  function flat(input, depth = 1, stack = []) {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const item of input) {
+      if (item instanceof Array && depth > 0) {
+        flat(item, depth - 1, stack);
+      } else {
+        stack.push(item);
+      }
+    }
+    return stack;
+  }
+
+  return flat(array);
 }
