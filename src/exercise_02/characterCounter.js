f7e730f074beb7ef2a581defe988415a3b26f6b2
Lingzhen Kong <1072476988@qq.com>
2020-09-09 15:17:31 +0800

    [lingzhen] task: update characterCounter.js.

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,21 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
+  if (string === null || string === undefined || string === '') {
+    return 0;
+  }
 
-  throw new Error('Please delete this line and implement the function');
+  if (prediction !== undefined) {
+    let count = 0;
+    const stringArray = string.split('');
+    stringArray.forEach((element) => {
+      if (prediction(element)) {
+        // eslint-disable-next-line no-plusplus
+        count++;
+      }
+    });
+    return count;
+  }
+
+  return string.length;
 }
