0acffc04bb39034f64e4c162b79566327ff883dc
Lingzhen Kong <1072476988@qq.com>
2020-09-09 16:59:38 +0800

    [lingzhen] task: update arrayToList.js.

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,18 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === null || undefined === array) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (Array.isArray(array) && array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+
+  function createList(n) {
+    if (n === array.length - 1) {
+      return { value: array[array.length - 1], next: null };
+    }
+    return { value: array[n], next: createList(n + 1) };
+  }
+  return createList(0);
 }
